d0acefa58edcffe8d9709139bb965202703034e5
Sanidhya Patel <127196048+lmaotrips@users.noreply.github.com>
2025-01-05 17:07:22 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+#print(len("hello"))
+#subscripting
+#print("hello"[4])
+#print(type(True))
+#print("number of letters in your name: " + str(len(input("?"))))
+ 
+#weight = 1.65
+#height = 84
+weight = input("what is your weight?")
+print(weight)
+height = input("what is your height?")
+print(height)
+BMI = int(weight) /int(height) ** 2
+print(BMI)
